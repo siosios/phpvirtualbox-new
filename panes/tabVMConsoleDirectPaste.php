3b745435a23eaee6ea3b16e7a338646ffeec398a
Semyon "xRef <mamayadesu@gmail.com>
2024-04-15 16:12:16 +0500

    Fixed direct paste auto focus

--- panes/tabVMConsoleDirectPaste.php
+++ panes/tabVMConsoleDirectPaste.php
@@ -5,7 +5,7 @@
     <form>
         <p><b><span class="translate">Direct paste</span></b></p>
         <p><label><input type='checkbox' id='vboxTabVMConsoleDirectPasteAsPassword' class='vboxCheckbox' /> <span class='translate'>Paste as password</span></label></p>
-        <input type="text" class="vboxText" id="vboxDirectPasteInput" style="width: 95%;">
+        <input type="text" class="vboxText" autocomplete="off" autofocus="yes" id="vboxDirectPasteInput" style="width: 95%;">
     </form>
 </div>
 <script type='text/javascript'>
@@ -16,6 +16,8 @@
         e.preventDefault();
         return false;
     });
+
+    $('#vboxDirectPasteInput').focus();
 
     $('#vboxTabVMConsoleDirectPasteAsPassword').change(function() {
         if (this.checked) {
