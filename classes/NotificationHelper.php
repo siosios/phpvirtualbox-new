2a58aac453405b706e52812ff6c9d19e95f9379c
Semyon "xRef <mamayadesu@gmail.com>
2024-10-30 20:29:13 +0500

    Notification helper

--- classes/NotificationHelper.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-class NotificationHelper
-{
-    public static function onPasswordSendConfirmationCode(string $username, string $code, int $expires) {
-        file_put_contents("/home/semyon/phpvirtualbox/phpvirtualbox.log.txt", "'$username' '$code' '$expires'\n", FILE_APPEND);
-    }
-}
